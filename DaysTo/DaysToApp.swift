62cb2a1b3fb4cb275332394ad0ab16b9ae7d8b17
killianmathias <killianmathias360@gmail.com>
2026-04-05 10:52:11 +0200

    Initial Commit

---
+++ DaysTo/DaysToApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysToApp.swift
+//  DaysTo
+//
+//  Created by Killian Mathias on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct DaysToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
